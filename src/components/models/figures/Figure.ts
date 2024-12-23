75047f54a1a56267bc02325b4cd28b16e74f6627
8olifirenko <8olifirenko@gmail.com>
2024-12-23 20:54:13 +0200

    add figures on board

--- src/components/models/figures/Figure.ts
+++ src/components/models/figures/Figure.ts
@@ -1,3 +1,37 @@
-export class Figure {
+import { Cell } from './../Cell';
+import { Colors } from "../Colors";
+import logo from "../../../assets/black-king.png"
 
+export enum FigureName {
+    FIGURE = "Figure",
+    KING = "King",
+    KNIGHT = "Knight",
+    PAWN = "Pawn",
+    QUEEN = "Queen",
+    ROOK = "Rook",
+    BISHOP = "Bishop",
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureName;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureName.FIGURE;
+        this.id = Math.random();
+    }
+
+    canMove(target: Cell): boolean {
+        return true;
+    }
+    moveFigure(target: Cell) {
+
+    }
 }
