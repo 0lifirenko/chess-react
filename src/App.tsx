12615ef1f5977be37af2285917dc197f51a2e358
8olifirenko <8olifirenko@gmail.com>
2025-01-03 21:49:58 +0200

    passing the turn to another player, restarting game

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react"
 import './App.css'
 import BoardComponent from "./components/BoardComponent";
-import Board from "./components/models/Board";
+import { Board } from "./components/models/Board";
+import { Player } from "./components/models/Player";
+import { Colors } from "./components/models/Colors";
 
 function App() {
   const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart()
+    setCurrentPlayer(whitePlayer);
   }, [])
 
   function restart() {
@@ -17,11 +23,17 @@ function App() {
     setBoard(newBoard)
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
   return (
     <div className='app'>
       <BoardComponent
         board={board}
         setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
       />
     </div>
   )
